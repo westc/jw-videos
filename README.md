04c873d51a0f2d5ec9cf4f5c067360bba3140fed
Chris West <cwest-engineer@outlook.com>
2017-05-09 15:42:48 -0400

    Add the icon made from https://codepen.io/pen?editors=0010

--- README.md
+++ README.md
@@ -20,5 +20,5 @@
 # jw-videos
 Show jw.org videos in bulk on your computer while preaching and keep track of how many videos were shown.
 
-![Filter on "Jehova"](/screenshots/details-7-videos.png?raw=true "Filter on Jehova")
-![Showing the details](/screenshots/details-7-videos.png?raw=true "Showing the details")
+![Filter on "Jehova"](/assets/screenshots/details-7-videos.png?raw=true "Filter on Jehova")
+![Showing the details](/assets/screenshots/details-7-videos.png?raw=true "Showing the details")
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+var path = require('path');
 var app = require('app');
 var BrowserWindow = require('browser-window');
 
 app.on('ready', function() {
-  var mainWindow = new BrowserWindow();
+  var mainWindow = new BrowserWindow({
+    icon: path.join(__dirname, 'assets/icons/JWVideos-128.png')
+  });
   mainWindow.maximize();
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file:///' + __dirname + '/index.html');
   mainWindow.on('close', function() {
     app.quit();
   });
